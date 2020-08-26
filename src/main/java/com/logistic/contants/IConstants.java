f34333f17b65cb33228445ee7ad827265ea467a7
denis.oliveira <denis.oliveira@colaborador.inpi.gov.br>
2020-08-26 16:11:31 -0300

    feat: Adicionado constantes

---
+++ src/main/java/com/logistic/contants/IConstants.java
@@ -0,0 +1,10 @@
+package com.logistic.contants;
+
+public interface IConstants {
+	String PACKAGE = "com.logistic";
+	String V1 = "v1/";
+	String ROOT_URL = "/api/";
+	
+	String CATEGORIA = ROOT_URL + V1 + "categorias";
+}
+
