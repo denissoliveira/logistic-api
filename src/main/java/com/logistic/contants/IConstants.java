ed803061fb9013690623327016ef2add6de522f1
denis.oliveira <denis.oliveira@colaborador.inpi.gov.br>
2020-08-26 16:20:29 -0300

    chore: Atualização e formatação

--- src/main/java/com/logistic/contants/IConstants.java
+++ src/main/java/com/logistic/contants/IConstants.java
@@ -2,8 +2,8 @@ package com.logistic.contants;
 
 public interface IConstants {
 	String PACKAGE = "com.logistic";
-	String V1 = "v1/";
 	String ROOT_URL = "/api/";
+	String V1 = "v1/";
 	
 	String CATEGORIA = ROOT_URL + V1 + "categorias";
 }
