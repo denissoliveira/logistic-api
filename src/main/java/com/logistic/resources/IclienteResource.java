816ea20fde555dee927d7ec720af60027788546f
denis.oliveira <denis.oliveira@colaborador.inpi.gov.br>
2020-08-26 23:58:50 -0300

    feat: Criado clienteResources

--- src/main/java/com/logistic/resources/IclienteResource.java
+++ src/main/java/com/logistic/resources/IclienteResource.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.logistic.contants.IConstants;
-import com.logistic.domain.Categoria;
+import com.logistic.domain.Cliente;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -15,14 +15,14 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-@RequestMapping(value = IConstants.CATEGORIAS)
-@Tag(name = "Categoria", description = "Categoria de produtos")
-public interface IcategoriaResource {
+@RequestMapping(value = IConstants.CLIENTES)
+@Tag(name = "Cliente", description = "Cliente de produtos")
+public interface IclienteResource {
 	
-	@Operation(summary = "Buscar Categoria", description = "Serviço Get para obter Categoria.")
+	@Operation(summary = "Buscar Cliente", description = "Serviço Get para obter Cliente.")
 	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "Categoria encontrada", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Categoria.class)) }),
-			@ApiResponse(responseCode = "404", description = "Categoria não encontrada", content = @Content)
+			@ApiResponse(responseCode = "200", description = "Cliente encontrado", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Cliente.class)) }),
+			@ApiResponse(responseCode = "404", description = "Cliente não encontrado", content = @Content)
 	})
 	@GetMapping(value = "/{id}")
 	ResponseEntity<?> find(@PathVariable Integer id);
