67a7132d195504c7ea45a4af6ebac4b2543f19ce
denis.oliveira <denis.oliveira@colaborador.inpi.gov.br>
2020-09-21 12:31:20 -0300

    feat: Excluindo um cliente sem pedidos

--- src/main/java/com/logistic/domain/Cliente.java
+++ src/main/java/com/logistic/domain/Cliente.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -25,7 +26,7 @@ public class Cliente extends BaseAudit {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	@ElementCollection
