b2351aebaaa85e7284e75e154fc90dfb6fcead00
denis.oliveira <denis.oliveira@colaborador.inpi.gov.br>
2020-11-16 19:31:22 -0300

    feat: restrição acesso GET

--- src/main/java/com/logistic/config/security/SecurityConfig.java
+++ src/main/java/com/logistic/config/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -26,8 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	//Endereços publicos que podem se acessados
 	private static final String[] PUBLIC_MATCHERS = {
-			"/h2-console/**",
-			"/api/v1/clientes/**"
+			"/h2-console/**"
 
 	};
 	
@@ -52,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
-			//.antMatchers(HttpMethod.GET,PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(HttpMethod.GET,PUBLIC_MATCHERS_GET).permitAll()
 			.anyRequest().authenticated();
 		
 		// Não vai criar a sessão do usuário
